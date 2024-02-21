e0c73668d4ec1bfac32c1edb1d244e1648514479
pomsky8 <fanigbrent3@gmail.com>
2024-02-21 18:11:13 +0800

    asias

---
+++ asias/resources/views/welcome.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head class="bg-violet-900">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('asset/css/home.css') }}">
+
+    <nav class="bg-violet-400">
+  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+    <div class="relative flex h-16 items-center justify-between">
+      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden ">
+        
+          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+          </svg>
+          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+      </div>
+
+      <div class="flex flex-5 items-center justify-center sm:items-stretch sm:justify-center">
+        <div class="flex flex-shrink-0 items-center">
+          <img class="h-8 w-auto justify-start" src="/assets/pictures/logo.png"><p class="text-slate-100">ASIA'S CHOCOLATES</p>
+        </div>
+
+        <div class="hidden sm:ml-6 sm:block ">
+          <div class="flex space-x-4 ">
+            <a href="#" class="bg-violet-200 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">HOME</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" >PRODUCTS</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">RESERVATIONS</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">ABOUT US</a>
+          </div>
+
+        </div>
+      </div>
+      
+    </div>
+  </div>
+
+</nav>
+</header>
+<body>
+
+<div class="mx-auto max-w-7xl py-96 sm:px-6 lg:px-9 bg-violet-300">
+    <div class="text-center font-bold text-6xl font-serif text-violet-900" style="margin-top: -10%;">
+        ASIA'S CHOCOLATES
+    </div>
+
+    <div class="font-medium text-center text-zinc-50 font-thin">
+    High quality chocolate products offer indulgent delights crafted with precision, <br> ensuring a rich and satisfying experience for chocolate enthusiasts.
+    </div>
+
+    <div class="button px-96">
+        <input class="btn btn-info bg-violet-600 rounded-lg " href="" type="submit" value="SHOP NOW" >
+    </div>
+      
+    </div>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
